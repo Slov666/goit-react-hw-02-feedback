c90c82c24f1f0fc653a45bfd79d0137a8848ae09
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-10-04 13:49:09 +0300

    Updates

---
+++ precache-manifest.3847b0dcd515efc5cb0e28857d4b32d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c040ae89026575ff3ee7272460a4f0",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "a3ee929061f9ddfe5919",
+    "url": "/goit-react-hw-02-feedback/static/css/main.184fe138.chunk.css"
+  },
+  {
+    "revision": "db303f605fce509f7f5c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8879fd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8879fd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ee929061f9ddfe5919",
+    "url": "/goit-react-hw-02-feedback/static/js/main.5419e29b.chunk.js"
+  },
+  {
+    "revision": "d62fa642c302dba33bcc",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.993c4773.js"
+  }
+]);
